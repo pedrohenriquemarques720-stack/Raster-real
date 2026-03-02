20323a4e428feca2dac58e4add8570c64fd19c8b
pedrohenriquemarques720-stack <pedrohenriquemarques720@gmail.com>
2026-03-02 08:25:54 -0300

    Update vehicle_profiles.py

--- vehicle_profiles.py
+++ vehicle_profiles.py
@@ -1,9 +1,8 @@
-# vehicle_profiles.py - Banco de dados de veículos
+# vehicle_profiles.py - Perfis de veículos
 
 class VehicleDatabase:
     """
     Banco de dados de perfis de veículos
-    Similar ao Autel - reconhece automaticamente
     """
     
     def __init__(self):
@@ -12,246 +11,265 @@ class VehicleDatabase:
     def _build_database(self):
         """Constrói banco de dados de veículos"""
         return {
-            # =========================================
-            # VOLKSWAGEN
-            # =========================================
             'VOLKSWAGEN': {
                 'Gol': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0', '1.6', '1.0 TSI'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'Polo': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0 TSI', '1.6', '200 TSI'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'Virtus': {
                     'years': [2020, 2021, 2022, 2023, 2024],
-                    'engines': ['1.0 TSI', '1.6', '200 TSI'],
+                    'engines': ['1.0 TSI', '1.6'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'T-Cross': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0 TSI', '1.4 TSI'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'Nivus': {
                     'years': [2021, 2022, 2023, 2024],
                     'engines': ['1.0 TSI'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
+                },
+                'Taos': {
+                    'years': [2022, 2023, 2024],
+                    'engines': ['1.4 TSI'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Bosch ME17.9.65'
                 }
             },
-            
-            # =========================================
-            # FIAT
-            # =========================================
             'FIAT': {
                 'Uno': {
                     'years': [2020, 2021, 2022, 2023, 2024],
-                    'engines': ['1.0', '1.3', '1.4'],
+                    'engines': ['1.0', '1.3'],
                     'protocol': 'KWP2000',
-                    'ecu': 'Magneti Marelli IAW',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F]
+                    'ecu': 'Magneti Marelli'
                 },
                 'Mobi': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0'],
                     'protocol': 'KWP2000',
-                    'ecu': 'Magneti Marelli IAW',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F]
+                    'ecu': 'Magneti Marelli'
                 },
                 'Argo': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0', '1.3', '1.8'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch'
                 },
                 'Cronos': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0', '1.3', '1.8'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch'
                 },
                 'Toro': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.3 Turbo', '1.8', '2.0 Diesel'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch EDC17',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch'
+                },
+                'Pulse': {
+                    'years': [2022, 2023, 2024],
+                    'engines': ['1.0', '1.3 Turbo'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Bosch'
                 }
             },
-            
-            # =========================================
-            # CHEVROLET
-            # =========================================
             'CHEVROLET': {
                 'Onix': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0 Turbo', '1.4'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'Onix Plus': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0 Turbo', '1.4'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'Tracker': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0 Turbo', '1.2 Turbo'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'Cruze': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.4 Turbo'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'S10': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['2.5', '2.8 Diesel'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch EDC17',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch EDC17'
                 }
             },
-            
-            # =========================================
-            # FORD
-            # =========================================
             'FORD': {
                 'Ka': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0', '1.5'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'Fiesta': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.0', '1.5', '1.6'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'EcoSport': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.5', '2.0'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch ME17.9.65',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch ME17.9.65'
                 },
                 'Ranger': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['2.2 Diesel', '3.2 Diesel'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch EDC17',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch EDC17'
+                },
+                'Territory': {
+                    'years': [2021, 2022, 2023, 2024],
+                    'engines': ['1.5 EcoBoost'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Bosch'
                 }
             },
-            
-            # =========================================
-            # TOYOTA
-            # =========================================
             'TOYOTA': {
                 'Corolla': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['2.0', '1.8 Hybrid'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Denso',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Denso'
                 },
                 'Hilux': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['2.4 Diesel', '2.8 Diesel'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Denso',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Denso'
                 },
                 'Yaris': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.3', '1.5'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Denso',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Denso'
+                },
+                'Etios': {
+                    'years': [2020, 2021, 2022, 2023],
+                    'engines': ['1.3', '1.5'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Denso'
+                },
+                'SW4': {
+                    'years': [2020, 2021, 2022, 2023, 2024],
+                    'engines': ['2.8 Diesel'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Denso'
                 }
             },
-            
-            # =========================================
-            # HONDA
-            # =========================================
             'HONDA': {
                 'Civic': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['2.0', '1.5 Turbo'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch'
                 },
                 'HR-V': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.5', '1.5 Turbo'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch'
                 },
                 'Fit': {
+                    'years': [2020, 2021, 2022, 2023],
+                    'engines': ['1.5'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Bosch'
+                },
+                'City': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.5'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch'
+                },
+                'CR-V': {
+                    'years': [2020, 2021, 2022, 2023, 2024],
+                    'engines': ['1.5 Turbo', '2.0 Hybrid'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Bosch'
                 }
             },
-            
-            # =========================================
-            # NISSAN
-            # =========================================
             'NISSAN': {
                 'Kicks': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.6'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Hitachi',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Hitachi'
                 },
                 'Versa': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['1.6'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Hitachi',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Hitachi'
                 },
                 'Frontier': {
                     'years': [2020, 2021, 2022, 2023, 2024],
                     'engines': ['2.3 Diesel', '2.5', '4.0'],
                     'protocol': 'CAN 500kbps',
-                    'ecu': 'Bosch EDC17',
-                    'pids_supported': [0x0C, 0x0D, 0x05, 0x0F, 0x10, 0x11]
+                    'ecu': 'Bosch EDC17'
+                },
+                'Sentra': {
+                    'years': [2020, 2021, 2022, 2023, 2024],
+                    'engines': ['2.0'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Hitachi'
+                }
+            },
+            'RENAULT': {
+                'Kwid': {
+                    'years': [2020, 2021, 2022, 2023, 2024],
+                    'engines': ['1.0'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Bosch'
+                },
+                'Sandero': {
+                    'years': [2020, 2021, 2022, 2023],
+                    'engines': ['1.0', '1.6'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Bosch'
+                },
+                'Duster': {
+                    'years': [2020, 2021, 2022, 2023, 2024],
+                    'engines': ['1.6', '2.0'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Bosch'
+                },
+                'Oroch': {
+                    'years': [2020, 2021, 2022, 2023, 2024],
+                    'engines': ['1.6'],
+                    'protocol': 'CAN 500kbps',
+                    'ecu': 'Bosch'
                 }
             }
         }
     
-    def identify_vehicle(self, vin):
+    def identify_vehicle(self, vin: str) -> Dict:
         """Identifica veículo pelo VIN"""
         if not vin or vin == '---':
             return None
@@ -266,15 +284,16 @@ class VehicleDatabase:
             '9BF': 'FORD',
             '935': 'FIAT',
             '9BD': 'FIAT',
-            '9BM': 'MERCEDES-BENZ',
-            '9BS': 'SCANIA',
             '93R': 'RENAULT',
-            '9GK': 'KIA',
-            '9HB': 'HONDA',
-            '9FB': 'RENAULT',
-            '9GA': 'PEUGEOT',
             '9GD': 'TOYOTA',
-            '9GN': 'NISSAN'
+            '9HB': 'HONDA',
+            '9GN': 'NISSAN',
+            '9GK': 'KIA',
+            '9GA': 'PEUGEOT',
+            '9GB': 'CITROEN',
+            '9GT': 'MITSUBISHI',
+            '9BM': 'MERCEDES',
+            '9BS': 'SCANIA'
         }
         
         manufacturer = wmi_map.get(wmi, 'DESCONHECIDO')
@@ -294,8 +313,8 @@ class VehicleDatabase:
             'year': year
         }
     
-    def get_profile(self, manufacturer, model):
-        """Retorna perfil completo do veículo"""
+    def get_profile(self, manufacturer: str, model: str) -> Dict:
+        """Retorna perfil do veículo"""
         if manufacturer in self.vehicles:
             if model in self.vehicles[manufacturer]:
                 return self.vehicles[manufacturer][model]
